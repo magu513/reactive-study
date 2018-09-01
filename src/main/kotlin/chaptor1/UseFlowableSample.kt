5643d57afd75f805e48ae35930b36a0349d327cd
magu <s.yamaguti513@gmail.com>
2018-09-02 00:16:11 +0900

    いくつかのサンプルを追加

--- src/main/kotlin/chaptor1/UseFlowableSample.kt
+++ src/main/kotlin/chaptor1/UseFlowableSample.kt
@@ -2,8 +2,10 @@ package chaptor1
 
 import io.reactivex.BackpressureStrategy
 import io.reactivex.Flowable
+import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.rxkotlin.Flowables
 import io.reactivex.schedulers.Schedulers
+import io.reactivex.subscribers.ResourceSubscriber
 import org.reactivestreams.Subscriber
 import org.reactivestreams.Subscription
 import java.util.concurrent.TimeUnit
@@ -128,6 +130,62 @@ fun main(args: Array<String>) {
 				override fun onError(t: Throwable?) {
 					println("エラー: $t")
 				}
-
 			})
+	useDisposableAndUnSubscribe()
+	useSubscribeWith()
+	useCompositeDisposable()
+}
+
+/**
+ * Disposableを使って購読を解除するサンプル
+ */
+fun useDisposableAndUnSubscribe() {
+	val disposable = Flowable.just(1,2,3).subscribe { println("data=$it")}
+	// 購読を解除する
+	disposable.dispose()
+}
+
+/**
+ * subscribeWithメソッドを使ってDisposableを取得する例
+ */
+fun useSubscribeWith() {
+	// subscribeWithはSubscriber/Observerを引数に取る
+	// subscribeWithは内部で引数に渡したsubscribeを実行し、引数をそのまま戻り値として返す。
+	// ResourceSubscriberはDisposableを実装しているため、Disposableとして受け取ることができる
+	val disposable = Flowable.range(1,3).subscribeWith(object: ResourceSubscriber<Int>() {
+		override fun onComplete() {}
+
+		override fun onNext(t: Int?) { println("data=$t")}
+
+		override fun onError(t: Throwable?) {}
+
+	})
+}
+
+/**
+ * CompositeDisposableを使ったサンプル
+ */
+fun useCompositeDisposable() {
+	// CompositeDisposableは複数のDisposableを同時に操作することができる
+	val compositeDisposable = CompositeDisposable()
+	compositeDisposable.add(Flowable.range(1, 3)
+			// 購読解除された際の処理
+			.doOnCancel { println("No.1 canceled") }
+			.observeOn(Schedulers.computation())
+			.subscribe {
+				Thread.sleep(100L)
+				println("No.1 : $it")
+			})
+
+	compositeDisposable.add(Flowable.range(5, 10)
+			.doOnCancel { println("No.2 canceled") }
+			// 計算用のThreadで処理を実行する
+			.observeOn(Schedulers.computation())
+			.subscribe {
+				Thread.sleep(100L)
+				println("No.2 : $it")
+			})
+
+	Thread.sleep(150L)
+	compositeDisposable.dispose()
 }
