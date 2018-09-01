e9bdfc0056c193abc65ec41c9cf6451c0813bc0d
magu <s.yamaguti513@gmail.com>
2018-09-01 16:55:51 +0900

    購読を途中で解除するサンプルの追加

--- src/main/kotlin/chaptor1/UseFlowableSample.kt
+++ src/main/kotlin/chaptor1/UseFlowableSample.kt
@@ -1,11 +1,15 @@
 package chaptor1
 
 import io.reactivex.BackpressureStrategy
+import io.reactivex.Flowable
 import io.reactivex.Scheduler
 import io.reactivex.rxkotlin.Flowables
+import io.reactivex.rxkotlin.subscribeBy
 import io.reactivex.schedulers.Schedulers
 import org.reactivestreams.Subscriber
 import org.reactivestreams.Subscription
+import sun.rmi.runtime.Log
+import java.util.concurrent.TimeUnit
 
 fun main(args: Array<String>) {
 	// 挨拶の言葉を通知するFlowableの生成
@@ -50,4 +54,32 @@ fun main(args: Array<String>) {
 				}
 			})
 	Thread.sleep(500L)
+
+	// 途中で購読を解除する
+	Flowable.interval(100L, TimeUnit.MILLISECONDS)
+			.subscribe(object: Subscriber<Long> {
+				private lateinit var subscription: Subscription
+				private var startTime: Long = 0L
+
+				override fun onSubscribe(s: Subscription?) {
+					this.subscription = s as Subscription
+					this.startTime = System.currentTimeMillis()
+					this.subscription.request(Long.MAX_VALUE)
+				}
+
+				override fun onNext(t: Long?) {
+					// 購読解除後に500ミリ秒後に購読解除
+					if ((System.currentTimeMillis() - startTime) > 500) {
+						subscription.cancel()
+						println("購読解除")
+						return
+					}
+
+					println("data=$t")
+				}
+				override fun onComplete() { }
+				override fun onError(t: Throwable?) { }
+
+			})
+	Thread.sleep(2000L)
 }
