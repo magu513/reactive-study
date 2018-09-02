c3c2686267b8105aafac91e659ad349aa9171f80
magu <s.yamaguti513@gmail.com>
2018-09-03 00:21:54 +0900

    flatMapメソッド内で異なるスレッド場で動くFlowableを生成した場合のサンプルを追加

---
+++ src/main/kotlin/chaptor3/UseOperatorSample.kt
@@ -0,0 +1,21 @@
+package chaptor3
+
+import io.reactivex.Flowable
+import java.util.concurrent.TimeUnit
+
+fun main(args: Array<String>) {
+	useFlatMap()
+}
+
+fun useFlatMap() {
+	// FlatMapは通知されたデータを同時に実行する
+	val flowable = Flowable.just("A", "B", "C")
+			// 受け取ったデータからFlowableを生成、通知する
+			.flatMap {
+				// 1000ms後にデータを通知するFlowableを生成
+				Flowable.just(it).delay(1000L, TimeUnit.MILLISECONDS)
+			}
+
+	flowable.subscribe { println("${Thread.currentThread().name} : $it") }
+	Thread.sleep(2000L)
+}
